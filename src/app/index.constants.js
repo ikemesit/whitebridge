2de4e5396fb653850a4ecbc2640f7763f3356ca4
ikemesit <ikemesit@gmail.com>
2016-05-14 02:13:55 +0800

    Initial Commit

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('whitebridge')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
