71d677da85fd4151f816c643df416e1f2117d381
ikemesit <ikemesit@gmail.com>
2016-05-16 09:14:43 +0800

    Update:further layout updates

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -13,6 +13,20 @@
         templateUrl: 'app/main/main.html',
         controller: 'MainController',
         controllerAs: 'main'
+      })
+
+      .state('about', {
+        url: '/about-us',
+        templateUrl: 'app/main/about.html',
+        controller: 'MainController',
+        controllerAs: 'main'
+      })
+
+      .state('services', {
+        url: '/our-services',
+        templateUrl: 'app/main/services.html',
+        controller: 'MainController',
+        controllerAs: 'main'
       });
 
     $urlRouterProvider.otherwise('/');
